232c1879b1b11420e64f1780e4b252a8846a36b8
rotaruandreeageorgiana-sudo <rotaruandreeageorgiana@gmail.com>
2026-03-05 20:28:34 +0200

    Update verify.js

--- api/verify.js
+++ api/verify.js
@@ -7,17 +7,39 @@ export default async function handler(req, res) {
 
 const { session_id } = req.query;
 
-const session = await stripe.checkout.sessions.retrieve(session_id);
+const session = await stripe.checkout.sessions.retrieve(session_id, {
+expand: ["line_items"]
+});
 
 if (session.payment_status !== "paid") {
 return res.status(403).send("Plata nu este confirmată");
 }
 
+const product = session.line_items.data[0].description;
+
+let file = "";
+
+if(product.includes("Volumul 1")){
+file = "vol1";
+}
+
+if(product.includes("Volumul 2")){
+file = "vol2";
+}
+
+if(product.includes("Volumul 3")){
+file = "vol3";
+}
+
+if(product.includes("Trilogie")){
+file = "trilogie";
+}
+
 const token = crypto.randomUUID();
 
 await kv.set(token, {
-used: false,
-file: "trilogie"
+used:false,
+file:file
 });
 
 res.redirect(/success.html?token=${token});
